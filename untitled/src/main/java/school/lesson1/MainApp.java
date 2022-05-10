3f4a010d273354d75d9a078c0dd781d9fc29d8ea
Yanina666 <RUZETKA-BESPAROLKA@rambler.ru>
2022-05-10 23:36:44 +0300

    compareNumbers

--- untitled/src/main/java/school/lesson1/MainApp.java
+++ untitled/src/main/java/school/lesson1/MainApp.java
@@ -3,15 +3,14 @@ package school.lesson1;
 
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println("printColor");
-        int value = 33;
-        if (value <= 0) {
-            System.out.println("Красный");
-        }
-        if (value > 0 & value <= 100) {
-            System.out.println("Желтый");}
-            if (value > 100) {
-                System.out.println("Зеленый");
-            }
+        System.out.println("compareNumbers");
+        int a = 33;
+        int b = 5;
+        if (a >= b) {
+            System.out.println("a>=b");
+        } if (a<b) {
+                System.out.println("a<b");
         }
     }
+}
+
